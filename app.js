63da5c42a5b8ab7a28afe48ae96d43b8236b75a3
eallen <elise.allen@op.ac.nz>
2024-04-07 23:49:29 +0000

    Add test data

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+// app.get("/", (req, res) => res.type('html').send(html));
 
-// About page route.
-app.get("/about", function (req, res) {
-  res.send("About this wiki");
+// Home page route.
+app.get("/", function (req, res) {
+  res.send("Hello World");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
